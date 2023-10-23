dd1ea0adeecdadbaa41cfa564227fe9ea6aca03a
DAVINA SINGH <davina_singh@outlook.com>
2023-10-23 15:23:14 +0100

    changes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>An animal with horns</p>
+        </div>
+    );
+}
+
+export default Footer;
